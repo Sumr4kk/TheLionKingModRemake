809e62d8fb2886d20b96c2b18858437fbec5efe8
Sumr4kk <alimbeckir@yandex.ru>
2026-02-10 23:13:02 +0300

    Logo add

---
+++ src/main/java/com/sumrakk/thelionkingremake/TheLionKingRemake.java
@@ -0,0 +1,59 @@
+package com.sumrakk.thelionkingremake;
+
+import com.sumrakk.thelionkingremake.items.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(TheLionKingRemake.MOD_ID)
+public class TheLionKingRemake
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "thelionkingremake";
+
+    public TheLionKingRemake() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::enqueueIMC);
+        eventBus.addListener(this::processIMC);
+        eventBus.addListener(this::processClient);
+
+        ModItems.register(eventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void processClient(final InterModProcessEvent event) {
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+
+    }
+}
---
+++ src/main/java/com/sumrakk/thelionkingremake/items/ModItems.java
@@ -0,0 +1,18 @@
+package com.sumrakk.thelionkingremake.items;
+
+import com.sumrakk.thelionkingremake.TheLionKingRemake;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TheLionKingRemake.MOD_ID);
+    public static final RegistryObject<Item> LOGO = ITEMS.register("logo", () -> new Item(new Item.Properties().group(ItemGroup.DECORATIONS)));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
+
